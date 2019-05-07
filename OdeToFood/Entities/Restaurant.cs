0af81fea5bdbac166d596e37d168cf27d8c2db75
mroyski <mroyski@gmail.com>
2019-05-06 20:05:59 -0400

    added home/create view and html helper form

--- OdeToFood/Entities/Restaurant.cs
+++ OdeToFood/Entities/Restaurant.cs
@@ -5,9 +5,19 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Entities
 {
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        French,
+        Japanese,
+        American
+    }
+
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
