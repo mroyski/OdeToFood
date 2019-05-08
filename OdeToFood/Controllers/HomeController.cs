48bc3206bc1a778a705865347e91f8ff0b5ad348
mroyski <mroyski@gmail.com>
2019-05-07 21:19:24 -0400

    added redirect into create post method

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -51,7 +51,7 @@ namespace OdeToFood.Controllers
 
             newRestaurant = _restaurantData.Add(newRestaurant);
 
-            return View("Details", newRestaurant);
+            return RedirectToAction("Details", new { id = newRestaurant.Id });
         }
     }
 }
